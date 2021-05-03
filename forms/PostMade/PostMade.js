42010d77d7089666840c0a0dbfc59f676489f7e7
amjoseph18 <abrahamjoseph@creighton.edu>
2021-05-03 13:04:27 -0500

    fixes

--- forms/PostMade/PostMade.js
+++ forms/PostMade/PostMade.js
@@ -2,3 +2,6 @@
 if (usernameTaken == false) {
         query = "INSERT INTO user (userName, password, first_name, last_name) VALUES ('"+userName+"','"+password+"','"+first_name+"','"+last_name+"')"
 */
+Image6.onclick=function(){
+    ChangeForm(loginPage)
+}
